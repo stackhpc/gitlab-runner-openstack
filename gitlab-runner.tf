9717189db466b1ac52faef705061373d4ed62973
Matt Pryor <matt@stackhpc.com>
2022-07-27 12:18:34 +0100

    Add additional options for Docker

--- gitlab-runner.tf
+++ gitlab-runner.tf
@@ -31,6 +31,12 @@ variable "network_id" {
   description = "The ID of the network to attach the runner to."
 }
 
+variable "docker_bip" {
+  type = string
+  description = "The CIDR to use for the Docker bridge."
+  default = "172.17.0.1/16"
+}
+
 variable "gitlab_host" {
   type = string
   description = "The host of the GitLab server to connect to."
@@ -57,6 +63,18 @@ variable "docker_image" {
   type = string
   description = "Default Docker image for the executor."
   default = "ubuntu:focal"
+}
+
+variable "docker_network_mode" {
+  type = string
+  description = "The Docker network mode to use for the executor. One of 'bridge' or 'host'."
+  default = "host"
+}
+
+variable "docker_privileged" {
+  type = bool
+  description = "Indicates whether to use privileged containers for the executor."
+  default = false
 }
 
 variable "register_extra_args" {
@@ -92,11 +110,14 @@ resource "openstack_compute_instance_v2" "node" {
     "${path.root}/install.sh.tftpl",
     {
       name = var.name,
+      docker_bip = var.docker_bip,
       gitlab_host = var.gitlab_host,
       scheme = var.scheme,
       registration_token = var.registration_token,
       ca_file = var.ca_file,
       docker_image = var.docker_image,
+      docker_network_mode = var.docker_network_mode,
+      docker_privileged = var.docker_privileged,
       register_extra_args = var.register_extra_args
     }
   )
